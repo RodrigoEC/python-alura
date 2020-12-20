6646ac3863197c5b11931068d73daf165ff7d15d
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-12-20 16:33:39 -0300

    Transferencia do codigo do jogos para uma funçao

--- jogos.py
+++ jogos.py
@@ -4,18 +4,22 @@ jogos = {
     '2': adivinhacao
 }
 
-print("++++++++++++++++++++++++++++++++++++++++")
-print("+++ Bem-vinde ao menu dos jogos!!!!! +++")
-print("++++++++++++++++++++++++++++++++++++++++")
-print()
+def escolhe_jogo():
+    print("++++++++++++++++++++++++++++++++++++++++")
+    print("+++ Bem-vinde ao menu dos jogos!!!!! +++")
+    print("++++++++++++++++++++++++++++++++++++++++")
+    print()
 
-print('''
-1 - Forca
-2 - Adivinhacao
-''')
+    print('''
+    1 - Forca
+    2 - Adivinhacao
+    ''')
 
-jogo = input('digite o numero do jogo que você quer jogar: ')
-try:
-    print(jogos[jogo]())
-except:
-    print("Você selecionou uma opcoção invalida!")
+    jogo = input('digite o numero do jogo que você quer jogar: ')
+    try:
+        print(jogos[jogo]())
+    except:
+        print("Você selecionou uma opcoção invalida!")
+
+if __name__ == "__main__":
+    escolhe_jogo()
