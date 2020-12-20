bce860401eac03d9f5314724c5c738b0e029a1c4
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-12-20 11:36:27 -0300

    Utilizaçao do 'continue' para pular uma iteraçao

--- modulo01/jogo_adivinhacao.py
+++ modulo01/jogo_adivinhacao.py
@@ -11,7 +11,11 @@ saida = 'Acabaram as tentativas! O número secreto era >> 42 <<.'
 
 for rodada in range(1, n_chances + 1):
     print(f'rodada {rodada} de {n_chances}')
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite o seu número entre 1 e 100: "))
+
+    if chute < 1 or chute > 100:
+        print("Meu amor...eu disse entre 1 e 100, perdesse essa rodada!")
+        continue
 
     if chute == n_secreto:
         saida = f'Você acertou e ainda sobraram {n_chances} tentativas! O número secreto era >> 42 <<'
