85bc32948c2123b0e5b0c646257b56e7d0d08861
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-12-20 11:47:25 -0300

    interpolaçao de strings com .format

---
+++ modulo01/interpolacao_strings.py
@@ -0,0 +1,15 @@
+batata = 1.56
+salpicao = 5.78
+bacia = 10.5
+
+print('>> Usando float')
+print('preço: {:04.2f}'.format(batata))
+print('preço: {:06.1f}'.format(salpicao))
+print('preço: {:07.2f}'.format(bacia))
+
+print()
+print('>> Inteiros')
+
+print('preço: {:02d}'.format(int(batata)))
+print('preço: {:06d}'.format(int(salpicao)))
+print('preço: {:07d}'.format(int(bacia)))
