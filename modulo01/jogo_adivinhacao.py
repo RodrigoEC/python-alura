ec0af4f7ac311d78b13872eb266e84d57d8f5b99
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-12-20 11:19:18 -0300

    Aula 02 - Repetiçoes no jogo de adivinhaçao

--- modulo01/jogo_adivinhacao.py
+++ modulo01/jogo_adivinhacao.py
@@ -1,12 +1,30 @@
 print("++++++++++++++++++++++++++++++++")
 print("Bem vindo ao jogo de adivinhação")
 print("++++++++++++++++++++++++++++++++")
+print()
+
 
 n_secreto = 42
-chute = int(input("Digite o seu número: "))
+n_chances = 10
 
-saida = f"Você errou! o número secreto era {n_secreto}"
-if n_secreto == chute:
-    saida = f"Você acertou!"
+saida = 'Acabaram as tentativas! O número secreto era >> 42 <<.'
+
+rodada = 0
+while (n_chances != rodada):
+
+    print(f'rodada {rodada} de {n_chances}')
+    chute = int(input("Digite o seu número: "))
+
+    if chute == n_secreto:
+        saida = f'Você acertou e ainda sobraram {n_chances} tentativas! O número secreto era >> 42 <<'
+        break
+    
+
+    if chute < n_secreto:
+        print('O número secreto é >> maior << do que o seu chute!')
+    else:
+        print('O número secreto é >> menor << do que o seu chute!')
+
+    rodada += 1
 
 print(saida)
