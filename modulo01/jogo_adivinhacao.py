535ba6ccec6dc6f199c6450ba2d3b82a4a7a0b86
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2020-12-20 11:26:44 -0300

    Usando for para fazer o joguinho

--- modulo01/jogo_adivinhacao.py
+++ modulo01/jogo_adivinhacao.py
@@ -9,22 +9,18 @@ n_chances = 10
 
 saida = 'Acabaram as tentativas! O número secreto era >> 42 <<.'
 
-rodada = 0
-while (n_chances != rodada):
-
+for rodada in range(1, n_chances + 1):
     print(f'rodada {rodada} de {n_chances}')
     chute = int(input("Digite o seu número: "))
 
     if chute == n_secreto:
         saida = f'Você acertou e ainda sobraram {n_chances} tentativas! O número secreto era >> 42 <<'
         break
-    
 
-    if chute < n_secreto:
+    elif chute < n_secreto:
         print('O número secreto é >> maior << do que o seu chute!')
     else:
         print('O número secreto é >> menor << do que o seu chute!')
 
-    rodada += 1
 
 print(saida)
